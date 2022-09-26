306866ac49bbe3dbd6c8c40ad2ddebc791c00d14
GhaziGithub <gqj3893@autuni.ac.nz>
2022-09-26 13:24:28 +1300

    article submission form, and articles display"

---
+++ frontend/src/components/articleDisplay.js
@@ -0,0 +1,173 @@
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+import { useTable, useSortBy, usePagination } from 'react-table';
+import tablecolumns from "../components/tablecolumns.js";
+import Styles from './tablestyles.js';
+import MainPage from './mainpage.js';
+const Table = ({columns, data}) => {
+const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    
+    page, // Instead of using 'rows', we'll use page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    
+    useSortBy,
+    usePagination
+  )
+    return (
+        <>
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                      {column.render('Header')}
+                      {/* Add a sort direction indicator */}
+                      <span>
+                        {column.isSorted
+                          ? column.isSortedDesc
+                            ? ' ￿'
+                            : ' ￿'
+                          : ''}
+                      </span>
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((row, i) => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>  
+         {/* Pagination */}
+         <div className="pagination">
+            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+              {'<<'}
+            </button>{' '}
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+              {'<'}
+            </button>{' '}
+            <button onClick={() => nextPage()} disabled={!canNextPage}>
+              {'>'}
+            </button>{' '}
+            <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+              {'>>'}
+            </button>{' '}
+            <span>
+              Page{' '}{' '}
+            </span>
+            <span>
+              | Go to page:{' '}
+              <input
+                type="number"
+                defaultValue={pageIndex + 1}
+                onChange={e => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0
+                  gotoPage(page)
+                }}
+                style={{ width: '100px' }}
+              />
+            </span>{' '}
+            <select
+              value={pageSize}
+              onChange={e => {
+                setPageSize(Number(e.target.value))
+              }}
+            >
+              {[3, 7, 15].map(pageSize => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+        </>
+      )
+    };
+
+
+      
+
+function DisplayArticles(){
+
+    let [articlesList,setArticlesList] = useState([])
+    let [selection,setSelection] = useState({data:'',value:''}); //creating a selection state, to control user selection
+    useEffect(()=>{
+        axios.get('http://localhost:8082/').then((res)=>{
+            if(selection.value===''){
+                setArticlesList(
+                    res.data
+            ) 
+            }
+            //if there is no selection (default) , all data will be displayed
+            else{
+            let data = res.data.filter((data)=>data.method===selection.value)
+            setArticlesList(
+                    data
+            )
+            }
+            //otherwise, filter data based on selection.
+
+        })
+    },[selection.value]) //adding selection.value as a variable for the dependency array, so that useEffect
+    //is invoked everytime a user select an option.
+
+    const onSelectionChange = (e)=>{
+        setSelection(
+            {
+                ...selection,
+                value:e.target.value
+            }
+        )
+        console.log("test")
+
+    }
+    // The basic idea here is to create a state array to store all information coming from the database. useEffect()
+    // will render only once after the page is fully rendered.
+
+    return(
+        <div className='main-page'>
+            <Styles>
+                <MainPage></MainPage>
+                <select onChange={(e)=>{onSelectionChange(e)}}>
+                    <option  value=''>Select an option...</option>
+                    <option value='TDD'>TDD</option>
+                    <option value='Mob Programming'>Mob Programming</option>
+                </select>
+                <Table
+            data={articlesList}
+            columns={tablecolumns}
+            />
+            </Styles>
+            </div>
+            )
+    }
+    
+export default DisplayArticles;
---
+++ frontend/src/components/articleSubmit.js
@@ -0,0 +1,87 @@
+
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import { useState} from "react";
+import MainPage from "./mainpage";
+
+
+
+
+function Articles(){
+        const [content,setContent] = useState({title:'',authors:'',volume:'',journal:'',number:'',pages:'',source:'',pubyear:'',doi:'',claim:'',evidence:''})
+        const [openDialog,setOpenDialog] = useState({message:'',value:0})
+        const {message,value} = openDialog 
+        const {title,authors,journal,number,pages,doi,pubyear,source,evidence,claim,volume} = content
+        const redirect = useNavigate()
+
+
+
+    const handleChange = (e)=>{
+        setContent(
+                {
+                ...content,
+                [e.target.name]:e.target.value
+                }
+        )
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        axios.post('http://localhost:8082/articles',content).then((res)=>{
+            setOpenDialog(
+                {
+                    value:!value,
+                    message:res.data
+
+                }
+
+            )
+            setTimeout(()=>{
+                redirect('/')
+            },3000)
+            //a dialog will display, and after 3 seconds the client will redirect the user to 'Home' page.
+           
+
+        })
+
+        
+    }
+
+
+
+    return(
+            <div className='article-div'>
+                <MainPage></MainPage>
+               
+                <div className='post-form'>
+    
+                <form method='post' className='post-page' onSubmit={(e)=>handleSubmit(e)}>
+                <div className='header'><h1>Article Submission Form</h1></div>
+                    <input required name='title' value={title} className='title' placeholder='insert the title' onChange={(e)=>handleChange(e)}/>
+                    <input required name='authors' value={authors} className='title' placeholder='insert the authors name/s' onChange={(e)=>handleChange(e)}/>
+                    <input name='source' value={source} className='title' placeholder="insert the source url" onChange={(e)=>handleChange(e)}/>
+
+                    <input name='volume' value={volume} className='volume' onChange={(e)=>handleChange(e)} placeholder='enter volume'/>
+                    <input name='pages' value={pages} className='pages' placeholder="insert number of pages" onChange={(e)=>handleChange(e)}/>
+                    <input name='journal' value={journal} className='journal' placeholder="insert journal" onChange={(e)=>handleChange(e)}/>
+                    <input name='pubyear' value={pubyear} className='pubyear' placeholder='insert publish year' onChange={(e)=>handleChange(e)}/>
+                    <input name='doi' value={doi} className='doi' placeholder='insert doi' onChange={(e)=>handleChange(e)}/>
+                    <input name='number' value={number} className='number' placeholder='insert the number' onChange={(e)=>handleChange(e)}/>
+                    <input name='claim' value={claim} className='claim' placeholder='insert the claim' onChange={(e)=>handleChange(e)}/>
+                    <input name="evidence" value={evidence} className='evidence' placeholder="insert the evidence" onChange={(e)=>handleChange(e)}/>
+                    <form method='dialog'>
+                    <dialog open={value} >
+                        {message}
+                    </dialog>
+                    </form>
+                    <button type='submit' className='submit'>Submit</button>                    
+                </form>
+                </div>
+
+            </div>
+        )
+    }
+
+
+
+export default Articles;
